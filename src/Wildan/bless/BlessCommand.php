fb4e503fb4c7ff60341a5a68909f31db92da8f7b
will <112930040+Wildan-dev461@users.noreply.github.com>
2023-08-12 05:18:29 +0700

    Update BlessCommand.php

--- src/Wildan/bless/BlessCommand.php
+++ src/Wildan/bless/BlessCommand.php
@@ -12,25 +12,21 @@ use pocketmine\entity\effect\Effect;
 class BlessCommand extends PluginBase {
 
     public function onEnable(): void {
-        $this->getLogger()->info("Bless Plugin enabled.");
-    }
-
-    public function onDisable(): void {
-        $this->getLogger()->info("Bless Plugin disabled.");
+        $this->saveDefaultConfig();
     }
 
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
         if ($command->getName() === "bless") {
             if (!$sender instanceof Player) {
-                $sender->sendMessage("This command can only be used in-game.");
+                $sender->sendMessage($this->getConfig()->get("only-ingame-message"));
                 return true;
             }
 
             $player = $sender;
             if ($this->removeNegativeEffects($player)) {
-                $player->sendMessage("You have been blessed! Negative effects removed.");
+                $player->sendMessage($this->getConfig()->get("blessed-message"));
             } else {
-                $player->sendMessage("There is no bad effect to remove in your body.");
+                $player->sendMessage($this->getConfig()->get("no-bad-effect-message"));
             }
 
             return true;
@@ -49,4 +45,3 @@ class BlessCommand extends PluginBase {
         return $hasNegativeEffects;
     }
 }
-
